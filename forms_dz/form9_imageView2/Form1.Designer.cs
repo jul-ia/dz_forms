7eb9d40ac0217ec9e43bd0420063f33c2636cbc6
wispeckiy <max.vitvitckiy@gmail.com>
2018-06-17 17:47:00 +0300

    ImageView2

---
+++ forms_dz/form9_imageView2/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace form9_imageView2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(53, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(533, 313);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(53, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse";
+            this.toolTip1.SetToolTip(this.button1, "Вибір папки");
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(220, 348);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "<";
+            this.toolTip1.SetToolTip(this.button2, "Назад");
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(323, 348);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = ">";
+            this.toolTip1.SetToolTip(this.button3, "Вперед");
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 427);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image View";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ forms_dz/form9_imageView2/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace form9_imageView2
+{
+    public partial class Form1 : Form
+    {
+        List<string> imgList = new List<string>();
+        int nImg = 0;
+        int pbh, pbw, pbx, pby;
+        string aPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pbh = pictureBox1.Height;
+            pbw = pictureBox1.Width;
+            pbx = pictureBox1.Location.X;
+            pby = pictureBox1.Location.Y;
+
+            DirectoryInfo di;
+            di = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures));
+            aPath = di.FullName;
+
+            FillListBox(aPath);
+        }
+
+        private Boolean FillListBox(string aPath)
+        {
+            DirectoryInfo di = new DirectoryInfo(aPath);
+            FileInfo[] fi = di.GetFiles("*.jpg");
+            imgList.Clear();
+
+            foreach (FileInfo fc in fi)
+                imgList.Add(fc.Name);
+
+            if (fi.Length == 0)
+                return false;
+            else
+            {
+                nImg = 0;
+                ShowPicture(aPath + "\\" + imgList[nImg]);
+                button2.Enabled = false;
+                if (imgList.Count == 1)
+                    button3.Enabled = false;
+
+                this.Text = aPath;
+                return true;
+            }
+        }
+
+        private void ShowPicture(string aPicture)
+        {
+            double mh, mw;
+            pictureBox1.Visible = false;
+            pictureBox1.Left = pbh;
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.Image = new Bitmap(aPicture);
+            if(pictureBox1.Image.Width > pbw || pictureBox1.Image.Height > pbh)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                mh = (double)pbh / (double)pictureBox1.Image.Height;
+                mw = (double)pbw / (double)pictureBox1.Image.Width;
+                if(mh < mw)
+                {
+                    pictureBox1.Width = Convert.ToInt16(pictureBox1.Image.Width * mh);
+                    pictureBox1.Height = pbh;
+                }
+                else
+                {
+                    pictureBox1.Width = pbw;
+                    pictureBox1.Height = Convert.ToInt16(pictureBox1.Image.Height * mw);
+                }
+            }
+            pictureBox1.Left = pbx + (pbw - pictureBox1.Width) / 2;
+            pictureBox1.Top = pby + (pbh - pictureBox1.Height) / 2;
+            pictureBox1.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!button3.Enabled)
+                button3.Enabled = true;
+            if(nImg > 0)
+            {
+                nImg--;
+                ShowPicture(aPath + "\\" + imgList[nImg]);
+                if (nImg == 0)
+                    button2.Enabled = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!button2.Enabled) button2.Enabled = true;
+            if (nImg < imgList.Count)
+            {
+                nImg++;
+                ShowPicture(aPath + "\\" + imgList[nImg]);
+                if (nImg == imgList.Count - 1)
+                {
+                    button3.Enabled = false;
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fb = new FolderBrowserDialog();
+            fb.Description = "Виберіть папку, \nв якій знаходяться ілюстраціі";
+            fb.ShowNewFolderButton = false;
+
+            fb.SelectedPath = aPath;
+            if (fb.ShowDialog() == DialogResult.OK)
+            {
+                aPath = fb.SelectedPath;
+                if (!FillListBox(fb.SelectedPath)) pictureBox1.Image = null;
+            }
+        }
+
+    }
+}
