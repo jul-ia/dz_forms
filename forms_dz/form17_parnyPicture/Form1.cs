b0b7a4f15e3f976aebae5644fb554fadb16f534b
jul-ia <julie78sd@gmail.com>
2018-06-24 18:13:48 +0300

    parny1

---
+++ forms_dz/form17_parnyPicture/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form17_parnyPicture
+{
+    public partial class Form1 : Form
+    {
+        const int nw = 4;
+        const int nh = 4;
+        const int np = (nw * nh) / 2;
+        System.Drawing.Graphics g;
+        Bitmap pics;
+        int cw, ch;
+        int[,] field = new int[nw, nh];
+
+        int nopened = 0;
+        int copened = 0;
+        int[] open1 = new int[2];
+        int[] open2 = new int[2];
+        System.Windows.Forms.Timer timer1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
